344be2a51632972c91bdc71996a7141288d3aaca
Ido <tihonist@DESKTOP-QAUJMNG>
2025-09-28 12:30:08 +0300

    Fixed messed up images in RolePage

--- ViewModels/RolePageViewModel.cs
+++ ViewModels/RolePageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using regmock.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace regmock.ViewModels
 {
@@ -12,9 +14,24 @@ namespace regmock.ViewModels
         #endregion
 
         #region Commands
+        public ICommand StudentClick_Cmd { get; set; }
+        public ICommand TeacherClick_Cmd { get; set; }
+        public ICommand PrincipalClick_Cmd { get; set; }
         #endregion
 
         #region Constructor
+        public RolePageViewModel()
+        {
+            StudentClick_Cmd = new Command(async () => {
+                await Shell.Current.Navigation.PushModalAsync(new RolePageStudent(), true);
+            });
+            TeacherClick_Cmd = new Command(async () => {
+                await Shell.Current.Navigation.PushModalAsync(new RolePageTeacher(), true);
+            });
+            PrincipalClick_Cmd = new Command(async () => {
+                await Shell.Current.Navigation.PushModalAsync(new RolePagePrincipal(), true);
+            });
+        }
         #endregion
 
         #region Functions
